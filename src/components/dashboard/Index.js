f698d42a646ea831c3f98e0eff0e1e1570b50a40
arizonv <a.vasgarridoe@gmail.com>
2023-04-15 14:31:12 -0400

    3.0

--- src/components/dashboard/Index.js
+++ src/components/dashboard/Index.js
@@ -1,13 +1,13 @@
-function Report() {
+function Index() {
     return (
       <div>
         <div className="container">
-          <h1>Report</h1>
+          <h1>Index</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi officia vero ad, incidunt voluptatibus praesentium, quo exercitationem, labore cum perferendis tenetur voluptates. Adipisci odio, commodi debitis, voluptatibus nostrum repudiandae porro amet qui cupiditate totam neque nihil ab voluptates at doloribus veniam. Voluptates quaerat ipsum dicta perspiciatis ea, temporibus esse architecto porro quis voluptatibus, laborum similique expedita officiis minima in quod officia accusamus beatae blanditiis minus repellendus adipisci distinctio. Esse voluptatibus nemo voluptate magni officiis eius laboriosam pariatur quia. Accusantium consectetur ipsam quae tenetur illum, ratione dolorem quidem corrupti voluptatem repellendus nam ipsum beatae id voluptatibus, provident labore pariatur sequi? Adipisci exercitationem, itaque quos odio cupiditate suscipit reiciendis praesentium consequuntur sapiente distinctio necessitatibus! Totam laborum neque, aut obcaecati tempora quod ab delectus atque quis aperiam necessitatibus mollitia harum officia excepturi. Asperiores, expedita ipsa. Ipsam sed, doloremque veritatis maiores ab provident est adipisci. Reprehenderit aliquid amet rem sequi cupiditate sapiente cum qui nobis, omnis laboriosam, minus quis odit incidunt dolor est quasi dicta, fuga optio atque a harum ratione! Nostrum suscipit blanditiis qui quasi voluptas corrupti et quod laudantium repellat. Optio dolores voluptatum corporis ut rem asperiores quas, reprehenderit molestiae autem quo? Voluptate excepturi delectus dolorem consectetur laborum et tempora aperiam?</p>
         </div>
       </div>
     );
   }
   
-  export default Report;
+  export default Index;
   
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Routes, Route, Outlet,Link } from 'react-router-dom';
 import Report from "../components/dashboard/Report";
 import Test from "../components/dashboard/Test"
+import Index from "../components/dashboard/Index"
+
+
 function Dashboard() {
   return (
     <div>
             <ul>
-                <li><Link to={"frontend"}>1- Frontenddd</Link></li>
-                <li><Link to={"backend"}>2- Backend</Link></li>
-                <li><Link to={"test"}>2- Backend</Link></li>
+                <li><Link to={"Report"}>2- Report</Link></li>
+                <li><Link to={"test"}>3- Test</Link></li>
             </ul>
             <Outlet/>
-            <Routes >
-                <Route path="frontend" element={<p>FrontEnd ✨</p>}/>
-                <Route path="backend" exact element={ <Report/> } />
+            <Routes>
+                <Route index exact element={ <Index/> } />
+                <Route path="/*" exact element={ <Index/> } />
+                <Route path="Report" exact element={ <Report/> } />
                 <Route path="test" exact element={ <Test/> } />
             </Routes>
         </div>
